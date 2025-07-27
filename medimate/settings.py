a57f27aa433c3ffe262e3128808710098ab6b63a
T Harsh Vardhan Singh <thvs.166@gmail.com>
2025-07-27 08:43:08 +0530

    Fix Render deploy config

--- medimate/settings.py
+++ medimate/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-(5%cx9)()vd%th#1+usn$b2)8f!)d8mteum)8i443iwi(1wh-t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['medimate.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['medimate-bjeu.onrender.com', '127.0.0.1', 'localhost']
 
 
 
